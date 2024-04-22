e8eb2ac1e821459e2c5f19df932fd2ec30d4c5c4
Somya Bhatt <somyacu1@gmail.com>
2024-04-23 03:36:09 +0530

    fix

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -2,7 +2,7 @@ import os from 'os'
 import path from 'path'
 
 export const folderName = 'qk';
-export const localAppDataPath = process.env.LOCALAPPDATA || os.platform()? '~./config' : os.homedir();
+export const localAppDataPath = process.env.LOCALAPPDATA || os.homedir();
 export const defaultDirPath = path.join(localAppDataPath, folderName);
 export const defaultDataPath = path.join(defaultDirPath, 'data.json');
 
